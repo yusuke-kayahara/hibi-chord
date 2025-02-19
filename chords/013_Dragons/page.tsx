9741f1173c76a713271a5ee964672ce4b13c4254
kyhrysk <yuu.dsk.0814@gmail.com>
2025-02-19 18:07:33 +0900

    新曲の追加

--- chords/013_Dragons/page.tsx
+++ chords/013_Dragons/page.tsx
@@ -1,63 +1,63 @@
 
 export default function ChordPage() {
 return (
-<div className="p-4 font-mono">
-  <h1 className="text-3xl font-bold">龖</h1>
-  <hr className="mb-4" />
-  <table className="table-auto">
-    <tbody>
-      <tr>
-        <td className="border px-2 py-1">Tuning</td>
-        <td className="border px-2 py-1">半音下げ</td>
-      </tr>
-      <tr>
-        <td className="border px-2 py-1">Original Key</td>
-        <td className="border px-2 py-1">Ab</td>
-      </tr>
-      <tr>
-        <td className="border px-2 py-1">Play Key</td>
-        <td className="border px-2 py-1">A</td>
-      </tr>
-    </tbody>
-  </table>
-  <p className="leading-[2em]">
-    <br />
-    <span className="relative text-[0.8em] text-blue-500 -top-[1em]">N.C.</span><br />
-    <br />
-    (<span className="relative text-[0.8em] text-blue-500 -top-[1em]">DM9</span>----<span className="relative text-[0.8em] text-blue-500 -top-[1em]">Bm9(11)</span>----<span className="relative text-[0.8em] text-blue-500 -top-[1em]">A</span>----<span className="relative text-[0.8em] text-blue-500 -top-[1em]">F#m7</span>--<span className="relative text-[0.8em] text-blue-500 -top-[1em]">DM9/G</span>--)*2<br />
-    <br />
-    <span className="relative text-[0.8em] text-blue-500 -top-[1em]">DM9</span>無音　その<span className="relative text-[0.8em] text-blue-500 -top-[1em]">Bm9(11)</span>中で<br />
-    鼓動は<span className="relative text-[0.8em] text-blue-500 -top-[1em]">A</span>速く　急<span className="relative text-[0.8em] text-blue-500 -top-[1em]">F#m7</span>かされてい<span className="relative text-[0.8em] text-blue-500 -top-[1em]">DM9/G</span>る<br />
-    <span className="relative text-[0.8em] text-blue-500 -top-[1em]">DM9</span>赤く染まる<span className="relative text-[0.8em] text-blue-500 -top-[1em]">Bm9(11)</span>体温<br />
-    <span className="relative text-[0.8em] text-blue-500 -top-[1em]">A</span>語り合い　触<span className="relative text-[0.8em] text-blue-500 -top-[1em]">F#m7</span>れ合う<span className="relative text-[0.8em] text-blue-500 -top-[1em]">DM9/G</span>光　<br />
-    <br />
-    <span className="relative text-[0.8em] text-blue-500 -top-[1em]">DM9</span>都合　目を<span className="relative text-[0.8em] text-blue-500 -top-[1em]">Bm9(11)</span>開いた<br />
-    軌道を逸れ<span className="relative text-[0.8em] text-blue-500 -top-[1em]">A</span>る　<span className="relative text-[0.8em] text-blue-500 -top-[1em]">F#m7</span>新<span className="relative text-[0.8em] text-blue-500 -top-[1em]">DM9/G</span>しく<br />
-    <span className="relative text-[0.8em] text-blue-500 -top-[1em]">DM9</span>もう　この手<span className="relative text-[0.8em] text-blue-500 -top-[1em]">Bm9(11)</span>には<br />
-     何<span className="relative text-[0.8em] text-blue-500 -top-[1em]">A</span>も <span className="relative text-[0.8em] text-blue-500 -top-[1em]">F#m7</span>遺せない<span className="relative text-[0.8em] text-blue-500 -top-[1em]">DM9/G</span><br />
-    <br />
-    <span className="relative text-[0.8em] text-blue-500 -top-[1em]">DM7</span>空洞だから<span className="relative text-[0.8em] text-blue-500 -top-[1em]">C#m7</span>覚えていない<br />
-    <span className="relative text-[0.8em] text-blue-500 -top-[1em]">F#m7</span>感覚だけで<span className="relative text-[0.8em] text-blue-500 -top-[1em]">Em7</span>描いた<span className="relative text-[0.8em] text-blue-500 -top-[1em]">A7</span>色<br />
-    <span className="relative text-[0.8em] text-blue-500 -top-[1em]">DM7</span>想像からは<span className="relative text-[0.8em] text-blue-500 -top-[1em]">C#m7</span>遠くない　　<br />
-    <span className="relative text-[0.8em] text-blue-500 -top-[1em]">F#m7</span>風光　どうして途切れた？<br />
-    <br />
-    <span className="relative text-[0.8em] text-blue-500 -top-[1em]">DM7</span>妄想までは<span className="relative text-[0.8em] text-blue-500 -top-[1em]">C#m7</span>覚えていない<br />
-    <span className="relative text-[0.8em] text-blue-500 -top-[1em]">F#m7</span>感覚だけで<span className="relative text-[0.8em] text-blue-500 -top-[1em]">Em7</span>話した<span className="relative text-[0.8em] text-blue-500 -top-[1em]">A7</span>こと<br />
-    <span className="relative text-[0.8em] text-blue-500 -top-[1em]">DM7</span>映像には<span className="relative text-[0.8em] text-blue-500 -top-[1em]">C#m7</span>残せない<br />
-    <span className="relative text-[0.8em] text-blue-500 -top-[1em]">F#m7</span>でも　描いた　描いた<br />
-    <br />
-    <span className="relative text-[0.8em] text-blue-500 -top-[1em]">DM7</span>----<span className="relative text-[0.8em] text-blue-500 -top-[1em]">C#m7</span>----<br />
-    <span className="relative text-[0.8em] text-blue-500 -top-[1em]">F#m7</span>空洞だから<span className="relative text-[0.8em] text-blue-500 -top-[1em]">Em7</span>覚えてい<span className="relative text-[0.8em] text-blue-500 -top-[1em]">A7</span>ない<br />
-    <span className="relative text-[0.8em] text-blue-500 -top-[1em]">DM7</span>----<span className="relative text-[0.8em] text-blue-500 -top-[1em]">C#m7</span>----<br />
-    <span className="relative text-[0.8em] text-blue-500 -top-[1em]">F#m7</span>感覚だけで描いた色<br />
-    <br />
-    <span className="relative text-[0.8em] text-blue-500 -top-[1em]">DM7</span>----<span className="relative text-[0.8em] text-blue-500 -top-[1em]">C#m7</span>----<br />
-    <span className="relative text-[0.8em] text-blue-500 -top-[1em]">F#m7</span>空洞だから<span className="relative text-[0.8em] text-blue-500 -top-[1em]">Em7</span>覚えてい<span className="relative text-[0.8em] text-blue-500 -top-[1em]">A7</span>ない<br />
-    <span className="relative text-[0.8em] text-blue-500 -top-[1em]">DM7</span>----<span className="relative text-[0.8em] text-blue-500 -top-[1em]">C#m7</span>----<br />
-    <span className="relative text-[0.8em] text-blue-500 -top-[1em]">F#m7</span>感覚だけで描いた色<br />
-    <br />
-    <span className="relative text-[0.8em] text-blue-500 -top-[1em]">DM7</span>
-  </p>
-</div>
+  <div className="p-4 font-mono">
+    <h1 className="text-3xl font-bold">龖</h1>
+    <hr className="mb-4" />
+    <table className="table-auto">
+      <tbody>
+        <tr>
+          <td className="border px-2 py-1">Tuning</td>
+          <td className="border px-2 py-1">半音下げ</td>
+        </tr>
+        <tr>
+          <td className="border px-2 py-1">Original Key</td>
+          <td className="border px-2 py-1">Ab</td>
+        </tr>
+        <tr>
+          <td className="border px-2 py-1">Play Key</td>
+          <td className="border px-2 py-1">A</td>
+        </tr>
+      </tbody>
+    </table>
+    <p className="leading-[2em]">
+      <br />
+      <span className="relative text-[0.8em] text-blue-500 -top-[1em]">N.C.</span><br />
+      <br />
+      (<span className="relative text-[0.8em] text-blue-500 -top-[1em]">DM9</span>----<span className="relative text-[0.8em] text-blue-500 -top-[1em]">Bm9(11)</span>----<span className="relative text-[0.8em] text-blue-500 -top-[1em]">A</span>----<span className="relative text-[0.8em] text-blue-500 -top-[1em]">F#m7</span>--<span className="relative text-[0.8em] text-blue-500 -top-[1em]">DM9/G</span>--)*2<br />
+      <br />
+      <span className="relative text-[0.8em] text-blue-500 -top-[1em]">DM9</span>無音　その<span className="relative text-[0.8em] text-blue-500 -top-[1em]">Bm9(11)</span>中で<br />
+      鼓動は<span className="relative text-[0.8em] text-blue-500 -top-[1em]">A</span>速く　急<span className="relative text-[0.8em] text-blue-500 -top-[1em]">F#m7</span>かされてい<span className="relative text-[0.8em] text-blue-500 -top-[1em]">DM9/G</span>る<br />
+      <span className="relative text-[0.8em] text-blue-500 -top-[1em]">DM9</span>赤く染まる<span className="relative text-[0.8em] text-blue-500 -top-[1em]">Bm9(11)</span>体温<br />
+      <span className="relative text-[0.8em] text-blue-500 -top-[1em]">A</span>語り合い　触<span className="relative text-[0.8em] text-blue-500 -top-[1em]">F#m7</span>れ合う<span className="relative text-[0.8em] text-blue-500 -top-[1em]">DM9/G</span>光　<br />
+      <br />
+      <span className="relative text-[0.8em] text-blue-500 -top-[1em]">DM9</span>都合　目を<span className="relative text-[0.8em] text-blue-500 -top-[1em]">Bm9(11)</span>開いた<br />
+      軌道を逸れ<span className="relative text-[0.8em] text-blue-500 -top-[1em]">A</span>る　<span className="relative text-[0.8em] text-blue-500 -top-[1em]">F#m7</span>新<span className="relative text-[0.8em] text-blue-500 -top-[1em]">DM9/G</span>しく<br />
+      <span className="relative text-[0.8em] text-blue-500 -top-[1em]">DM9</span>もう　この手<span className="relative text-[0.8em] text-blue-500 -top-[1em]">Bm9(11)</span>には<br />
+      何<span className="relative text-[0.8em] text-blue-500 -top-[1em]">A</span>も <span className="relative text-[0.8em] text-blue-500 -top-[1em]">F#m7</span>遺せない<span className="relative text-[0.8em] text-blue-500 -top-[1em]">DM9/G</span><br />
+      <br />
+      <span className="relative text-[0.8em] text-blue-500 -top-[1em]">DM7</span>空洞だから<span className="relative text-[0.8em] text-blue-500 -top-[1em]">C#m7</span>覚えていない<br />
+      <span className="relative text-[0.8em] text-blue-500 -top-[1em]">F#m7</span>感覚だけで<span className="relative text-[0.8em] text-blue-500 -top-[1em]">Em7</span>描いた<span className="relative text-[0.8em] text-blue-500 -top-[1em]">A7</span>色<br />
+      <span className="relative text-[0.8em] text-blue-500 -top-[1em]">DM7</span>想像からは<span className="relative text-[0.8em] text-blue-500 -top-[1em]">C#m7</span>遠くない　　<br />
+      <span className="relative text-[0.8em] text-blue-500 -top-[1em]">F#m7</span>風光　どうして途切れた？<br />
+      <br />
+      <span className="relative text-[0.8em] text-blue-500 -top-[1em]">DM7</span>妄想までは<span className="relative text-[0.8em] text-blue-500 -top-[1em]">C#m7</span>覚えていない<br />
+      <span className="relative text-[0.8em] text-blue-500 -top-[1em]">F#m7</span>感覚だけで<span className="relative text-[0.8em] text-blue-500 -top-[1em]">Em7</span>話した<span className="relative text-[0.8em] text-blue-500 -top-[1em]">A7</span>こと<br />
+      <span className="relative text-[0.8em] text-blue-500 -top-[1em]">DM7</span>映像には<span className="relative text-[0.8em] text-blue-500 -top-[1em]">C#m7</span>残せない<br />
+      <span className="relative text-[0.8em] text-blue-500 -top-[1em]">F#m7</span>でも　描いた　描いた<br />
+      <br />
+      <span className="relative text-[0.8em] text-blue-500 -top-[1em]">DM7</span>----<span className="relative text-[0.8em] text-blue-500 -top-[1em]">C#m7</span>----<br />
+      <span className="relative text-[0.8em] text-blue-500 -top-[1em]">F#m7</span>空洞だから<span className="relative text-[0.8em] text-blue-500 -top-[1em]">Em7</span>覚えてい<span className="relative text-[0.8em] text-blue-500 -top-[1em]">A7</span>ない<br />
+      <span className="relative text-[0.8em] text-blue-500 -top-[1em]">DM7</span>----<span className="relative text-[0.8em] text-blue-500 -top-[1em]">C#m7</span>----<br />
+      <span className="relative text-[0.8em] text-blue-500 -top-[1em]">F#m7</span>感覚だけで描いた色<br />
+      <br />
+      <span className="relative text-[0.8em] text-blue-500 -top-[1em]">DM7</span>----<span className="relative text-[0.8em] text-blue-500 -top-[1em]">C#m7</span>----<br />
+      <span className="relative text-[0.8em] text-blue-500 -top-[1em]">F#m7</span>空洞だから<span className="relative text-[0.8em] text-blue-500 -top-[1em]">Em7</span>覚えてい<span className="relative text-[0.8em] text-blue-500 -top-[1em]">A7</span>ない<br />
+      <span className="relative text-[0.8em] text-blue-500 -top-[1em]">DM7</span>----<span className="relative text-[0.8em] text-blue-500 -top-[1em]">C#m7</span>----<br />
+      <span className="relative text-[0.8em] text-blue-500 -top-[1em]">F#m7</span>感覚だけで描いた色<br />
+      <br />
+      <span className="relative text-[0.8em] text-blue-500 -top-[1em]">DM7</span>
+    </p>
+  </div>
 );
 }
